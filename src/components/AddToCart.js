48907c9c945686c66603f5bdf25caf81f9ec408e
Jorge Peres <jorge-peres@live.com>
2021-04-17 13:15:12 -0300

    GridView

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -8,6 +8,25 @@ import AmountButtons from './AmountButtons'
 const AddToCart = ({ product }) => {
   const { id, stock, colors } = product
   const [mainColor, setMainColor] = useState(colors[0])
+  const [amount, setAmount] = useState(1)
+  const increase = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1
+      if (tempAmount > stock) {
+        tempAmount = stock
+      }
+      return tempAmount
+    })
+  }
+  const decrease = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1
+      if (tempAmount < 1) {
+        tempAmount = 1
+      }
+      return tempAmount
+    })
+  }
 
   return (
     <Wrapper>
@@ -30,7 +49,16 @@ const AddToCart = ({ product }) => {
           })}
         </div>
       </div>
-      <div className='btn-container'></div>{' '}
+      <div className='btn-container'>
+        <AmountButtons
+          amount={amount}
+          increase={increase}
+          decrease={decrease}
+        />
+        <Link to='/cart' className='btn'>
+          add to cart
+        </Link>
+      </div>
     </Wrapper>
   )
 }
--- src/components/PageHero.js
+++ src/components/PageHero.js
@@ -7,8 +7,9 @@ const PageHero = ({ title, product }) => {
     <Wrapper>
       <div className='section-center'>
         <h3>
-          <Link to='/'>Home</Link>/{' '}
-          {product && <Link to='/products'>Products </Link>} /{title}
+          <Link to='/'>Home /</Link>
+          {product && <Link to='/products'>Products / </Link>}
+          {title}
         </h3>
       </div>
     </Wrapper>
